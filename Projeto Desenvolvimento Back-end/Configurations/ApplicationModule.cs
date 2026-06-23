7d0dbc790087e6f65a7945803d1aa3867b1e7bf2
Guilherme Kendi <kendicoding.contato@gmail.com>
2026-06-22 21:57:07 -0300

    First Commit: Organizaçao de pastas do projeto

---
+++ Projeto Desenvolvimento Back-end/Configurations/ApplicationModule.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Projeto_Desenvolvimento_Back_end.Configurations
+{
+    public static class ApplicationModule
+    {
+        public static IServiceCollection ConfigureDependecies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.ConfigureServices();
+            services.ConfigureRepositories();
+            return services;
+        }
+
+        private static void ConfigureServices(this IServiceCollection services)
+        {
+            
+        }
+
+        private static void ConfigureRepositories(this IServiceCollection services)
+        {
+
+        }
+    }
+}
