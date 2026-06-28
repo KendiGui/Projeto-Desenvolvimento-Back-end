8af2311dc21ce79828cb553b6a4eb3e15f9b64ce
Guilherme Kendi <Kendicoding.contato@gmail.com>
2026-06-28 17:54:10 -0300

    correcao de erro ao tentar salvar auditoria, setando o campo antes como nullable

--- Domain/Entities/Auditoria.cs
+++ Domain/Entities/Auditoria.cs
@@ -6,7 +6,7 @@ namespace Domain.Entities
         public string Acao { get; set; }
         public string Entidade { get; set; }
         public long? EntidadeId { get; set; }
-        public string DadosAntes { get; set; }
+        public string? DadosAntes { get; set; }
         public string DadosDepois { get; set; }
         public string Ip { get; set; }
 
--- Infrastructure/Migrations/20260628201735_DadosAntesAnulavel.Designer.cs
+++ Infrastructure/Migrations/20260628201735_DadosAntesAnulavel.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20260628201735_DadosAntesAnulavel")]
+    partial class DadosAntesAnulavel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
